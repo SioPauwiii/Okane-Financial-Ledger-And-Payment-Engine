bd86e708965a559439ce55b8e76cf8b6fe7339e8
SioPauwii <paulocordova3@gmail.com>
2026-06-09 16:50:15 +0800

    init

---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { useColorScheme } from 'react-native';
+
+import { AnimatedSplashOverlay } from '@/components/AnimatedIcon';
+import AppTabs from '@/components/AppTabs';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AnimatedSplashOverlay />
+      <AppTabs />
+    </ThemeProvider>
+  );
+}
